40c8c8ca052067a99c52b96b23b35a242cc46d49
Dimitri Klimenko <d.klimenko@arkoselabs.com>
2022-12-06 02:18:56 +1000

    Day 5 Part 2

--- day5.hs
+++ day5.hs
@@ -4,15 +4,13 @@ import System.Environment (getArgs)
 import Data.Array (array, Array, listArray, elems, (//), (!))
 import Data.Bifunctor (first, bimap)
 import Data.Char (isSpace)
-import Data.List (transpose, dropWhile)
+import Data.List (transpose, dropWhile, splitAt, foldl')
 import Data.List.Split (chunksOf)
 import Data.Maybe (Maybe)
 
 import Utils (parseBlock, splitPairOn, mapPair)
 
-newtype Action = Action (Int, Int, Int)
-    deriving Show
-
+type Action = (Int, Int, Int)
 type State = Array Int String
 
 makeOneBasedArray :: [a] -> Array Int a
@@ -28,23 +26,31 @@ parseState :: String -> State
 parseState = makeOneBasedArray . map trimLeft . transpose . init . parseBlock parseRow
 
 parseAction :: String -> Action
-parseAction s = Action (read $ wds !! 1, read $ wds !! 3, read $ wds !! 5)
+parseAction s = (read $ wds !! 1, read $ wds !! 3, read $ wds !! 5)
     where wds = words s
 
-moveCrate :: Int -> Int -> State -> State
-moveCrate from to s = s // [(from, fs), (to, f:ts)]
-    where (f:fs) = s ! from
-          ts     = s ! to
+moveCrate :: (Int, Int) -> State -> State
+moveCrate (from, to) state = state // [(from, fs), (to, f:ts)]
+    where (f:fs) = state ! from
+          ts     = state ! to
 
-processActions :: (State, [Action]) -> (State, [Action])
-processActions (s, [])                      = (s, [])
-processActions (s, Action (0, from, to):xs) = processActions (s, xs)
-processActions (s, Action (n, from, to):xs) = processActions (moveCrate from to s, Action (n-1, from, to):xs)
+moveCrates1 :: State -> Action -> State
+moveCrates1 state (0, from, to) = state
+moveCrates1 state (n, from, to) = moveCrates1 (moveCrate (from, to) state) (n-1, from, to) 
 
-day5 :: (State, [Action]) -> String
-day5 = map head . elems . fst . processActions
+moveCrates2 :: State -> Action -> State
+moveCrates2 state (n, from, to) = state // [(from, f2), (to, f1 ++ ts)]
+    where (f1, f2) = splitAt n $ state ! from
+          ts       = state ! to
+
+processActions :: (State -> Action -> State) -> (State, [Action]) -> State
+processActions moveFunc (state, actions) = foldl' moveFunc state actions
+
+day5 :: (State -> Action -> State) -> (State, [Action]) -> String
+day5 moveFunc = map head . elems . processActions moveFunc
 
 main :: IO ()
 main = do
     args <- getArgs
-    getContents >>= putStrLn . day5 . bimap parseState (parseBlock parseAction) . splitPairOn "\n\n"
+    let moveFunc = (if null args then moveCrates1 else moveCrates2)
+    getContents >>= putStrLn . day5 moveFunc . bimap parseState (parseBlock parseAction) . splitPairOn "\n\n"
