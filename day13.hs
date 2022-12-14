5c3c7e85b8ccd8c70293e15783961d5fc20921ab
Dimitri Klimenko <d.klimenko@arkoselabs.com>
2022-12-14 23:38:22 +1000

    Day 13 Part 2

--- day13.hs
+++ day13.hs
@@ -1,11 +1,10 @@
 #!/usr/bin/env runhaskell
 
-import Data.List.Split (splitOn)
+import Data.List (findIndices, sort)
+import Data.List.Split (chunksOf, splitOn)
 import System.Environment (getArgs)
 import Text.ParserCombinators.ReadPrec (ReadPrec, choice)
 import Text.Read (Read, readPrec)
-
-import Utils (mapPair, splitPairOn)
 
 data Packet = Val Int | List [Packet]
   deriving (Eq)
@@ -26,13 +25,19 @@ instance Ord Packet where
   compare (Val x1)   (List ps2) = compare (List [Val x1]) (List ps2)
   compare (List ps1) (Val x2)   = compare (List ps1) (List [Val x2])
 
-parseSignal :: String -> [(Packet, Packet)]
-parseSignal = map (mapPair read . splitPairOn "\n") . splitOn "\n\n"
+parseList :: String -> [Packet]
+parseList = map read . filter (not . null) . lines
 
-sumOrderedIndices :: [(Packet, Packet)] -> Int
-sumOrderedIndices signal = sum [i | (i, (p1, p2)) <- zip [1..] signal, p1 <= p2]
+part1 :: [Packet] -> Int
+part1 packets = sum [i | (i, [p1, p2]) <- zip [1..] (chunksOf 2 packets), p1 <= p2]
+
+dividers :: [Packet]
+dividers = read "[ [[2]], [[6]] ]"
+
+part2 :: [Packet] -> Int
+part2 signal = product . map (+ 1) . findIndices (`elem` dividers) . sort $ signal ++ dividers
 
 main :: IO ()
 main = do
     args <- getArgs
-    getContents >>= print . sumOrderedIndices . parseSignal
+    getContents >>= print . (if null args then part1 else part2) . parseList
