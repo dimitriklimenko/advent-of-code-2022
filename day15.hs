328030314ea9ee12a38ca90e2c85e3f2368253c6
Dimitri Klimenko <d.klimenko@arkoselabs.com>
2022-12-16 03:15:28 +1000

    Style improvements

--- day15.hs
+++ day15.hs
@@ -1,12 +1,12 @@
 #!/usr/bin/env runhaskell
 
 import Data.Either.Combinators (fromRight')
-import Data.List (find, unzip4)
+import Data.List (find)
 import System.Environment (getArgs)
 import Text.Parsec (Parsec, endBy, many1, parse)
 import Text.Parsec.Char (newline, oneOf, string)
 
--- Types
+-- Types and constructors / destructors
 data Pos = Pos
     { x :: Int
     , y :: Int
@@ -18,7 +18,11 @@ newtype Beacon = Beacon { bPos :: Pos }
 data Sensor = Sensor
     { sPos :: Pos
     , nearestBeacon :: Beacon
+    , beaconDist :: Int
     } deriving (Eq, Show)
+
+makeSensor :: Pos -> Beacon -> Sensor
+makeSensor sp beacon = Sensor sp beacon (distance sp (bPos beacon))
 
 -- Parsing
 number :: Parsec String () Int
@@ -32,7 +36,7 @@ sensorParser = do
     string "Sensor at "
     sensorPos <- posParser
     string ": closest beacon is at "
-    Sensor sensorPos . Beacon <$> posParser
+    makeSensor sensorPos . Beacon <$> posParser
 
 inputParser :: Parsec String () [Sensor]
 inputParser = endBy sensorParser newline
@@ -41,37 +45,28 @@ inputParser = endBy sensorParser newline
 distance :: Pos -> Pos -> Int
 distance (Pos x1 y1) (Pos x2 y2) = abs (x2 - x1) + abs (y2 - y1)
 
-beaconDist :: Sensor -> Int
-beaconDist (Sensor sp (Beacon bp)) = distance sp bp
-
-distBounds :: Int -> Sensor -> (Int, Int, Int, Int)
-distBounds d (Sensor (Pos x y) _) = (x-d, x+d, y-d, y+d)
-
-combinedBounds :: [Sensor] -> (Int, Int, Int, Int)
-combinedBounds sensors = (minimum x1s, maximum x2s, minimum y1s, maximum y2s)
-  where
-    (x1s, x2s, y1s, y2s) = unzip4 . map (\s -> distBounds (beaconDist s) s) $ sensors
-
 outOfRange :: Pos -> Sensor -> Bool
-outOfRange newPos sensor@(Sensor sp _) = distance newPos sp > beaconDist sensor
+outOfRange newPos sensor@(Sensor sp _ d) = distance newPos sp > d
 
+-- Part 1 solution
 validForSensor :: Pos -> Sensor -> Bool
-validForSensor newPos sensor@(Sensor _ (Beacon bp)) = newPos == bp || outOfRange newPos sensor
+validForSensor newPos sensor@(Sensor _ (Beacon bp) _) = newPos == bp || outOfRange newPos sensor
 
 part1 :: Int -> [Sensor] -> Int
-part1 yMax sensors = sum . map fromEnum $ [not $ all (validForSensor (Pos x (yMax `div` 2))) sensors | x <- [x1..x2]]
+part1 yMax sensors = sum . map (fromEnum . (\pos -> not . all (validForSensor pos) $ sensors)) $ candidates
   where
-    (x1, x2, _, _) = combinedBounds sensors
+    y          = yMax `div` 2
+    x1         = minimum [ x - d | (Sensor (Pos x y) _ d) <- sensors]
+    x2         = maximum [ x + d | (Sensor (Pos x y) _ d) <- sensors]
+    candidates = [Pos x y | x <- [x1 .. x2]]
 
+-- Part 2 solution
 fringe :: Sensor -> [Pos]
-fringe sensor@(Sensor (Pos x y) _) =
-    zipWith Pos [x1..x] [y..y2] ++
-    zipWith Pos [x1..x] [y,y-1..y1] ++
-    zipWith Pos [x..x2] [y1..y] ++
-    zipWith Pos [x..x2] [y2,y2-1..y]
-  where
-    d                = beaconDist sensor
-    (x1, x2, y1, y2) = distBounds (d+1) sensor
+fringe sensor@(Sensor (Pos x y) _ d) =
+    zipWith Pos [x-d-1..x] [y..y+d+1] ++
+    zipWith Pos [x-d-1..x] [y,y-1..y-d-1] ++
+    zipWith Pos [x..x+d+1] [y-d-1..y] ++
+    zipWith Pos [x..x+d+1] [y+d+1,y+d..y]
 
 inBounds :: (Int, Int) -> Pos -> Bool
 inBounds (minVal, maxVal) (Pos x y) = x >= minVal && x <= maxVal && y >= minVal && y <= maxVal
@@ -80,7 +75,7 @@ part2 :: Int -> [Sensor] -> Int
 part2 yMax sensors = 4000000 * x + y
   where
     candidates     = filter (inBounds (0, yMax)) . concatMap fringe $ sensors
-    Just (Pos x y) = find (\x -> all (outOfRange x) sensors) candidates
+    Just (Pos x y) = find (\pos -> all (outOfRange pos) sensors) candidates
 
 main :: IO ()
 main = do
@@ -88,4 +83,3 @@ main = do
     let handler = if null args || (not . read . head) args then part1 else part2
     let yMax = if length args < 2 then 4000000 else read (args !! 1)
     getContents >>= print . handler yMax . fromRight' . parse inputParser ""
-
